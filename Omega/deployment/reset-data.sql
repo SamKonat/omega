40a7f65095eb119ce095e629f7865ac77d030ea8
Samuel <samuel.konat@gmail.com>
2018-09-29 22:39:42 -0400

    Data reset script

---
+++ Omega/deployment/reset-data.sql
@@ -0,0 +1,10 @@
+
+/**
+ * Author:  Sam
+ * Created: Sep 29, 2018
+ */
+
+DELETE FROM role_permission_map;
+DELETE FROM user_role;
+DELETE FROM access_permission;
+DELETE FROM access_permission_category;
