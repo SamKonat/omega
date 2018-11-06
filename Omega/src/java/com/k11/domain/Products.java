6ef5b58be34d9d907bbb87cfdde2a238a77d0782
dilishanaidu <44125457+dilishanaidu@users.noreply.github.com>
2018-11-05 19:19:02 -0500

    test commit

---
+++ Omega/src/java/com/k11/domain/Products.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.k11.domain;
+
+/**
+ *
+ * @author dilis
+ */
+public class Products {
+    private Long id;
+    private String productName;
+    private String description;
+    private String brandId;
+    private String image;
+    private Float price;
+    private Integer quantity;
+
+    public Products() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    @Override
+    public String toString() {
+        return "Products{" + "id=" + id + ", productName=" + productName + 
+                ", description=" + description + ", brandId=" + brandId + 
+                ", image=" + image + ", price=" + price + ", quantity=" + 
+                quantity + '}';
+    } 
+}
