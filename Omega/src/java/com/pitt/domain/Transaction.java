085d95326ffc8b168b3e55a8b7ad1b5512ad9c54
dilishanaidu <44125457+dilishanaidu@users.noreply.github.com>
2018-11-06 19:52:43 -0500

    Transaction POJO

---
+++ Omega/src/java/com/pitt/domain/Transaction.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pitt.domain;
+
+/**
+ *
+ * @author dilis
+ */
+public class Transaction {
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private Float price;
+    private String status;
+    private String deliveryStatus;
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" + "id=" + id + ", productId=" + productId + 
+                ", userId=" + userId + ", price=" + price + ", status=" + 
+                status + ", deliveryStatus=" + deliveryStatus + ", type=" 
+                + type + '}';
+    }
+    
+}
