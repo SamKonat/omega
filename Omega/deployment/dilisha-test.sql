82ab87fbd7ce4638bf758fb58aa695dbbcd40137
dilishanaidu <44125457+dilishanaidu@users.noreply.github.com>
2018-11-26 15:16:38 -0500

    Create dilisha-test.sql

---
+++ Omega/deployment/dilisha-test.sql
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Sam
+ * Created: Nov 20, 2018
+ */
+
+INSERT INTO omega_user(ou_id, ou_first_name, ou_last_name, ou_email, ou_ph_number,
+    ou_address, ou_role_id, ou_password) VALUES(1, 'Admin', 'Omega', 'admin@omega.com',
+    '4127730994', '3295 Kennett Square', 1, 'test');
+INSERT INTO omega_user(ou_id, ou_first_name, ou_last_name, ou_email, ou_ph_number,
+    ou_address, ou_role_id, ou_password) VALUES(2, 'Operator', 'Omega', 'operator@omega.com',
+    '4127730994', '3295 Kennett Square', 2, 'test');
+INSERT INTO omega_user(ou_id, ou_first_name, ou_last_name, ou_email, ou_ph_number,
+    ou_address, ou_role_id, ou_password) VALUES(3, 'Dilisha', 'Naidu', 'din10@pitt.edu',
+    '4127730994', '3295 Kennett Square', 3, 'test');
+INSERT INTO omega_user(ou_id, ou_first_name, ou_last_name, ou_email, ou_ph_number,
+    ou_address, ou_role_id, ou_password) VALUES(4, 'Sohini', 'Palit', 'soh12@pitt.edu',
+    '4127730994', '3295 Kennett Square', 3, 'test');
+INSERT INTO omega_user(ou_id, ou_first_name, ou_last_name, ou_email, ou_ph_number,
+    ou_address, ou_role_id, ou_password) VALUES(5, 'Samuel', 'Konat', 'sjk124@pitt.edu',
+    '4127730994', '3295 Kennett Square', 3, 'test');
